53455e238fdddc81992e2723fe42ede0114bdb0b
ljcool2006 <51834343+ljcool2006@users.noreply.github.com>
2020-07-01 17:04:07 -0400

    1.16.1

--- assets.py
+++ assets.py
@@ -1,5 +1,5 @@
 # Meta file used to obtain some of the vanilla assets (sounds/languages)
-# Get the textures from .minecraft/versions/1.14/1.14.jar
+# Get the textures from .minecraft/versions/1.16.1/1.16.1.jar
 import shutil
 import json
 import os
@@ -17,7 +17,7 @@ elif system.startswith('darwin'):
     destfolder = os.path.expanduser(os.path.join('~', 'Library', 'Application Support', 'minecraft'))
 elif system.startswith('win'):
     destfolder = os.path.expandvars(os.path.join('%APPDATA%', '.minecraft'))
-with open('/home/noahkiq/.minecraft/assets/indexes/1.14.json') as f:
+with open('/home/noahkiq/.minecraft/assets/indexes/1.16.1.json') as f:
     data = json.load(f)
 for key, value in data["objects"].items():
     if '/lang/' in key.lower() or '.ogg' in key.lower():
